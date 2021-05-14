08ca17b0d91365f9a44f2ef1069f2e577d9a5482
Deepakcnct <deepak.cnct@gmail.com>
2021-05-14 06:18:23 +0530

    microservices 2.4.5 initila commit

---
+++ limit-service/src/main/java/com/in28minutes/microservices/limitservice/controller/LimitsController.java
@@ -0,0 +1,21 @@
+package com.in28minutes.microservices.limitservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.microservices.limitservice.beans.Limits;
+import com.in28minutes.microservices.limitservice.configuiration.Configuration;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public Limits reterieveLimits() {
+		return new Limits(configuration.getMinimum(),configuration.getMaximum());
+	}
+}
